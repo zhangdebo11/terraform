4c536bea0cd7db02733f7722e3c2fceaf7314c32
zhangdebo11 <zhang_debo@c.tre-inc.com>
2023-03-23 14:46:52 +0800

    test

--- standard-cluster.tf
+++ standard-cluster.tf
@@ -9,6 +9,20 @@ resource "google_container_cluster" "primary" {
   initial_node_count       = 1
 
   node_config {
-    machine_type = "e2-standard-4"
+    # machine_type = "e2-standard-4"
+    machine_type = "e2-micro"
+  }
+
+  management {
+    auto_upgrade = false
+  }
+
+  private_cluster_config {
+    enable_private_nodes = true
+  }
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block = "10.90.128.0/17"
+    services_ipv4_cidr_block = "10.91.0.0/22"
   }
 }
