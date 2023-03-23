cc67e0a198a37df41af2b7108bbf9d1e31a9bb6f
zhangdebo11 <zhang_debo@c.tre-inc.com>
2023-03-23 11:10:16 +0800

    test

--- standard-cluster.tf
+++ standard-cluster.tf
@@ -1,18 +1,14 @@
 resource "google_container_cluster" "primary" {
-  name     = "standard-cluster-terraform"
+  name     = "staging-manju-melonpan-cluster-standard"
   location = "asia-northeast1"
   project  = "smartcart-stagingization"
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  remove_default_node_pool = true
+  network  = "default"
+  subnetwork = "staging-to-4u"
+
   initial_node_count       = 3
+
   node_config {
     machine_type = "e2-medium"
-    labels = {
-      foo = "bar"
-    }
-    tags = ["foo", "bar"]
   }
 }
