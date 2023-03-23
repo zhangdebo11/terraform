c791dc51f9ad52a30747ff5da26655fc96a9064a
zhangdebo11 <zhang_debo@c.tre-inc.com>
2023-03-23 17:22:54 +0800

    test

--- staging/staging.tf
+++ staging/staging.tf
@@ -1,15 +1,15 @@
 resource "google_container_cluster" "primary" {
-  name     = var.cluster_name
-  location = var.cluster_location
-  project  = var.cluster_project
+  name     = "staging-manju-melonpan-cluster-standard"
+  location = "asia-northeast1"
+  project  = "smartcart-stagingization"
 
-  network  = var.cluster_network
-  subnetwork = var.cluster_subnetwork
+  network  = "default"
+  subnetwork = "staging-to-4u"
 
   node_pool {
-    node_count = var.node_count
+    node_count = 1
     node_config {
-      machine_type = var.machine_type
+      machine_type = "e2-micro"
     }
     management {
       auto_upgrade = false
@@ -19,11 +19,11 @@ resource "google_container_cluster" "primary" {
 
   private_cluster_config {
     enable_private_nodes = true
-    master_ipv4_cidr_block = var.master_ipv4_cidr_block
+    master_ipv4_cidr_block = "172.16.10.0/28"
   }
 
   ip_allocation_policy {
-    cluster_ipv4_cidr_block = var.pod_ipv4_cidr_block
-    services_ipv4_cidr_block = var.services_ipv4_cidr_block
+    cluster_ipv4_cidr_block = "10.80.128.0/17"
+    services_ipv4_cidr_block = "10.81.0.0/22"
   }
 }
