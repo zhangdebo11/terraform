7f6baa21bdb30be41169736334104e37a01ee54b
zhangdebo11 <zhang_debo@c.tre-inc.com>
2023-03-29 17:49:11 +0800

    test

--- staging/manju-melonpan-cluster.tf
+++ staging/manju-melonpan-cluster.tf
@@ -1,12 +1,10 @@
-resource "google_container_cluster" "staging_manju" {
+resource "google_container_cluster" "primary" {
   name     = "staging-manju-melonpan-cluster-standard"
   location = "asia-northeast1"
   project  = "smartcart-stagingization"
 
   network  = "default"
   subnetwork = "staging-to-4u"
-
-  initial_node_count       = 1
 
   remove_default_node_pool = true
 
@@ -21,17 +19,16 @@ resource "google_container_cluster" "staging_manju" {
   }
 }
 
-resource "google_container_node_pool" "staging_manju" {
+resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "pool-1"
   location   = "asia-northeast1"
-  cluster    = google_container_cluster.staging_manju.name
+  cluster    = google_container_cluster.primary.name
   project    = "smartcart-stagingization"
 
-  node_count = 0
+  node_count = 1
 
   node_config {
-    # machine_type = "e2-standard-4"
-    machine_type = "e2-micro"
+    machine_type = "e2-standard-4"
   }
 
   management {
