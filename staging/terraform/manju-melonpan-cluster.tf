547ad84420736a56ea9e234f57a2456e3c73effa
zhangdebo11 <zhang_debo@c.tre-inc.com>
2023-03-30 17:29:40 +0800

    test

--- staging/terraform/manju-melonpan-cluster.tf
+++ staging/terraform/manju-melonpan-cluster.tf
@@ -14,9 +14,9 @@ resource "google_container_cluster" "primary" {
   }
 
   private_cluster_config {
-    enable_private_nodes = false
-    # enable_private_nodes = true
-    # master_ipv4_cidr_block = "172.16.10.0/28"
+    # enable_private_nodes = false
+    enable_private_nodes = true
+    master_ipv4_cidr_block = "172.16.10.0/28"
   }
 
   ip_allocation_policy {
@@ -57,6 +57,3 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     total_max_node_count = 100
   }
 }
-
-# 脚本每次重启都会执行一次
-# 集群自动伸缩，自动升级？
