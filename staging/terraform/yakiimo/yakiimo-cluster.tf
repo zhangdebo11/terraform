002923129a294158cde941b214a712ab1cf56903
zhangdebo11 <zhang_debo@c.tre-inc.com>
2023-04-14 10:30:50 +0800

    mm

--- staging/terraform/yakiimo/yakiimo-cluster.tf
+++ staging/terraform/yakiimo/yakiimo-cluster.tf
@@ -57,6 +57,6 @@ resource "google_container_node_pool" "yakiimo_preemptible_nodes" {
 
   autoscaling {
     total_min_node_count = 1
-    total_max_node_count = 100
+    total_max_node_count = 10
   }
 }
