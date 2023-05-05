c5460b53573c4efdfa2e643f5c6c062fa4d8cc9b
zhangdebo11 <zhang_debo@c.tre-inc.com>
2023-05-05 16:00:52 +0800

    mm

--- staging/terraform/yakiimo/yakiimo-cluster.tf
+++ staging/terraform/yakiimo/yakiimo-cluster.tf
@@ -37,59 +37,6 @@ resource "google_container_cluster" "yakiimo" {
   }
 }
 
-resource "google_container_node_pool" "yakiimo_preemptible_nodes_2" {
-  name       = "pool-2"
-  location   = "asia-northeast1"
-  cluster    = google_container_cluster.yakiimo.name
-  project    = "smartcart-stagingization"
-
-  node_config {
-    machine_type = "custom-6-6144"
-    image_type   = "UBUNTU_CONTAINERD"
-    disk_size_gb = 100
-    metadata = {
-      "startup-script-url" = "gs://staging-standard-cluster/system-init-script.sh"
-      "disable-legacy-endpoints" = "true"
-    }
-  }
-
-  management {
-    auto_upgrade = true
-    auto_repair = true
-  }
-
-  autoscaling {
-    total_min_node_count = 0
-    total_max_node_count = 20
-  }
-}
-
-resource "google_container_node_pool" "yakiimo_preemptible_nodes_3" {
-  name       = "pool-3"
-  location   = "asia-northeast1"
-  cluster    = google_container_cluster.yakiimo.name
-  project    = "smartcart-stagingization"
-
-  node_config {
-    machine_type = "custom-4-4096"
-    image_type   = "UBUNTU_CONTAINERD"
-    disk_size_gb = 100
-    metadata = {
-      "startup-script-url" = "gs://staging-standard-cluster/system-init-script.sh"
-      "disable-legacy-endpoints" = "true"
-    }
-  }
-
-  management {
-    auto_upgrade = true
-    auto_repair = true
-  }
-
-  autoscaling {
-    total_min_node_count = 0
-    total_max_node_count = 20
-  }
-}
 
 resource "google_container_node_pool" "yakiimo_preemptible_nodes_4" {
   name       = "pool-4"
